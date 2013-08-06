2b15e7156014f294e6abbdfd9af82d0c7756728d
Dustin Coates <dustin@dcoates.com>
2013-08-05 20:49:59 -0400

    Add initial class

---
+++ src/TermsWithChums.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dustin
+ * Date: 8/5/13
+ * Time: 8:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TermsWithChums {
+}
