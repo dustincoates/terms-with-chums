5a33041fa228d7a50e81883983d002eb89bc5d13
Dustin Coates <dustin@dcoates.com>
2013-09-02 22:54:53 -0400

    Clean up O(n) queries

--- src/User.java
+++ src/User.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,53 +12,73 @@ import java.util.HashMap;
  */
 public class User {
     public String name = "";
-    private ArrayList<Tile> tiles = new ArrayList<Tile>();
+    private HashMap<String, ArrayList<Tile>> tiles = new HashMap<String, ArrayList<Tile>>();
     private Integer points = 0;
 
     public User(String userName, TileBag tileBag){
         name = userName;
+
         for (int i = 0; i < 7; i++){
+            ArrayList<Tile> tileHolder = new ArrayList<Tile>();
             Tile tile = tileBag.getTile();
-            tiles.add(tile);
+            // Too much repetition with below. Combine/refactor
+            if (tiles.get(String.valueOf(tile.getLetter())) != null){
+                // The above and below can be combined -- refactor.
+                tileHolder = tiles.get(String.valueOf(tile.getLetter()));
+                tileHolder.add(tileHolder.size(),tile);
+            } else {
+                tileHolder.add(tile);
+            }
+            tiles.put(String.valueOf(tile.getLetter()),tileHolder);
         }
     }
 
     public String showTiles(){
         String tilesString = "| ";
-        for (int i = 0; i < tiles.size(); i++){
-            tilesString += tiles.get(i).getLetter();
-            tilesString += " | ";
+        Iterator it = tiles.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry)it.next();
+            for (int i = 0; i < tiles.get(pairs.getKey()).size(); i++){
+                tilesString += pairs.getKey();
+                tilesString += " | ";
+            }
+
+
         }
         return tilesString;
     };
 
     public void addTiles(ArrayList<Tile> tilesToAdd){
         for (int i = 0; i < tilesToAdd.size(); i++){
+            ArrayList<Tile> tileHolder = new ArrayList<Tile>();
             Tile tile = tilesToAdd.get(i);
-            tiles.add(tilesToAdd.get(i));
+            if (tiles.get(String.valueOf(tile.getLetter())) != null){
+                // The above and below can be combined -- refactor.
+                tileHolder = tiles.get(String.valueOf(tile.getLetter()));
+                tileHolder.add(tile);
+            } else {
+                tileHolder.add(tile);
+            }
+            tiles.put(String.valueOf(tile.getLetter()),tileHolder);
         }
     }
 
-    // Refactor this--currently O(n) but with something like a HashMap, it could be much faster
     public Tile getTile(char letter){
-        for(int i = 0; i<tiles.size(); i++){
-            if(letter == tiles.get(i).getLetter()){
-                Tile tileToReturn = tiles.get(i);
-                tiles.remove(i);
-                return tileToReturn;
-            }
+        if(checkLetter(letter)){
+            Tile tile = tiles.get(String.valueOf(letter)).get(0);
+            tiles.get(String.valueOf(letter)).remove(0);
+            return tile;
+        } else {
+            return null;
         }
-        return null;
     }
 
-    // Refactor this--currently O(n) but with something like a HashMap, it could be much faster
     public boolean checkLetter(char letter){
-        for(int i = 0; i <tiles.size(); i++){
-            if (letter == tiles.get(i).getLetter()){
-                return true;
-            }
+        if(tiles.get(String.valueOf(letter)) != null){
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
 
